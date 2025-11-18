972b8ea4fb20138d7a1bf35d49470b8d1445e78b
Jobeda Hossain <jobedanew393@gmail.com>
2025-11-18 23:20:07 +0600

    Lab_Performance

---
+++ Lab_Task_1.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        WT_O
+    </title>
+</head>
+<body>
+    <center>
+        <h1>Blood Donation Management System</h1>
+    </center>
+    <center>
+        <h2>Donate Blood And Save A Life </h2>
+    </center>
+    <h1>Blood Donation Form</h1>
+    <p>Welcome to Blood Donation Form<br><br>Kindly fillup the Information Correctly
+    <br><br>
+    <table>
+     <tr>
+     <td>Enter Your Name:</td>
+     <td><input type="text"></td>
+     </tr>
+    Enter Your Age:
+    <input type="number">
+    <br><br>
+    Enter Your Weight:
+    <input type="number">
+    <br><br>
+    Gender:
+    <input type="checkbox">Male
+    <input type="checkbox">Female
+    <br><br>
+    Enter Your DOB:
+    <input type="date">
+    <br><br>
+    Are you interested to donate Blood?:
+    <input type="checkbox">YES
+    <input type="checkbox">NO
+    <br><br>
+    Blood Group:
+    <input type="radio">A+
+    <input type="radio">A-
+    <input type="radio">B+
+    <input type="radio">B-
+    <input type="radio">O+
+    <input type="radio">0-
+    <input type="radio">AB+
+    <input type="radio">AB-
+ 
+    <br><br>
+    Enter Your Last Blood Donation Date:
+    <input type="date">
+   
+ 
+    <center><input type="submit"></center>
+    <br>
+    <center><input type="reset"></center>
+    </body>
+</html>
