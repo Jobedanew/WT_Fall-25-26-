b55cebfc48090c91980fe31fd90e1865333b90d6
Jobeda Hossain <jobedanew393@gmail.com>
2025-11-28 01:15:08 +0600

    education table has beed added

--- html_Lt-1.php
+++ html_Lt-1.php
@@ -3,11 +3,27 @@
     <head>
         <title>Portfolio</title>
     </head>
-    <body>
+    <body style=background-color:lavender;>
         <h1>Jobeda Hossain</h1>
+        
         <section>Hi! im jobeda. I'm currently pursuing my 
             Bechelor Degree in Computer Science and Engineering from AIUB.
         </section>
-        <h2>Heading 2</h2>
+        <br>
+        <table >
+            <tr>
+                <th>Education</th>
+                <th>GPA/CGPA</th>   
+            </tr>
+            
+            <tr>
+                <th>HSC</th>
+                <th>5.00</th>  
+            </tr>
+            <tr>
+                <th>Bsc</th>
+                <th>4.00</th>  
+            </tr>
+        
     </body>
 </html>
