d5e07a4bd65a2cf73246d66ee9b48ed0ec6a93ca
Jobeda Hossain <jobedanew393@gmail.com>
2025-11-27 15:28:22 +0600

    name as heading

---
+++ lab_2_odd.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>ODD</title>
+    </head>
+    <body>
+        <h2>Lets start it!</h2>
+        <p1>opps later!</p1>
+        <section>i dont know from where i can start :<</section>
+    </body>
+</html>
